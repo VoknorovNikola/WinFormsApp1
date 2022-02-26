fec289d98f147804366c80f1539fffeec2d0b8ce
VoknorovNikola <kolla95@mail.ru>
2022-02-27 00:53:18 +0300

    refactor
    action

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -6,25 +6,33 @@ namespace WinFormsApp1
         {
             InitializeComponent();
         }
+
         string[] textData;
-        bool modifyText = false;
+        bool modifyingText = false;
 
         public void textProcess()
         {
-            while (modifyText)
+            while (modifyingText)
             {
 
                Random Rand = new Random();
                int stringNumber = Rand.Next(0, textData.Length);
-               textData[stringNumber] = textData[stringNumber].Insert(new Random().Next(0, textData[stringNumber].Length), InsertTextInput.Text);
+               textData[stringNumber] = textData[stringNumber].Insert(Rand.Next(0, textData[stringNumber].Length), InsertTextInput.Text);
                if (textViewer.InvokeRequired)
                {
-                textViewer.Invoke(new Action(() => textViewer.Items.Clear()));
-                textViewer.Invoke(new Action(() => textViewer.Items.AddRange(textData)));
+                    var refreshTextView = new Action(() =>
+                    {
+                         textViewer.Items.Clear();
+                         textViewer.Items.AddRange(textData);
+                    }
+                    );
+
+                    textViewer.Invoke(refreshTextView);
+
                }
+
                Thread.Sleep(5000);
             }
-
         }
 
         private void LoadTextFileButton_Click(object sender, EventArgs e)
@@ -39,43 +47,40 @@ namespace WinFormsApp1
                 textViewer.Items.Clear();
                 textViewer.Items.AddRange(textData);
             }
-
         }
-
 
         private void StartStopButton_Click(object sender, EventArgs e)
         {
-            if (!modifyText)
+            if (!modifyingText)
             {
                if ((textViewer.Items.Count == 0)||(String.IsNullOrEmpty(InsertTextInput.Text)))
-                  MessageBox.Show("Невозможно запустить программу пока не будет загружен не пустой TXT файл и не заполненно поле ввода");
+                {
+                    MessageBox.Show("Невозможно запустить программу пока не будет загружен не пустой TXT файл и не заполненно поле ввода");
+                }
                else
-               {
+                {
                     StartStopButton.Text = "Стоп";
                     LoadTextFileButton.Enabled = false;
                     InsertTextInput.Enabled = false;
-                    modifyText = true;
+                    modifyingText = true;
                     Thread textProcessThread = new Thread(textProcess);
                     textProcessThread.IsBackground = true;
                     textProcessThread.Start();
-               }
+                }
             }
             else
             {
                 StartStopButton.Text = "Старт";
-                modifyText = false;
+                modifyingText = false;
                 LoadTextFileButton.Enabled = true;
                 InsertTextInput.Enabled = true;
-
             }
-
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
-            modifyText = false;
+            modifyingText = false;
 
         }
-
     }
 }
